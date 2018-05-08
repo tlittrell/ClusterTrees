afa09fae947b8efb3d740e4f2eaf541e94173196
timvalicenti <timvalicenti@gmail.com>
2018-05-08 16:13:44 -0400

    TestData

--- ClustTreeSynthData.jl
+++ ClustTreeSynthData.jl
@@ -1,8 +1,9 @@
 function create_synthetic_data(n,μ,σ;seed = 1234)
     srand(seed)
-    X = (μ[1].+σ[1]*randn(n[1],2)')'
+    p = size(μ[1])[1]
+    X = (μ[1].+σ[1]*randn(n[1],p)')'
     for i = 2:size(n)[1]
-        new_x = (μ[i].+σ[i]*randn(n[i],2)')'
+        new_x = (μ[i].+σ[i]*randn(n[i],p)')'
         X = vcat(X,new_x)
     end
     return X
